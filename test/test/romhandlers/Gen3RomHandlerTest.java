84fa407a7490f8909787b5119ae159985abfd3d3
voliol <30631214+voliol@users.noreply.github.com>
2023-02-09 22:48:14 +0100

    move to JUnit 5, write some basic test cases to try it out

--- test/test/romhandlers/Gen3RomHandlerTest.java
+++ test/test/romhandlers/Gen3RomHandlerTest.java
@@ -1,62 +1,86 @@
 package test.romhandlers;
 
 import com.dabomstew.pkrandom.constants.Gen3Constants;
-import com.dabomstew.pkrandom.constants.Species;
-import com.dabomstew.pkrandom.pokemon.Move;
-import com.dabomstew.pkrandom.pokemon.Pokemon;
 import com.dabomstew.pkrandom.romhandlers.Gen3RomHandler;
 import com.dabomstew.pkrandom.romhandlers.RomHandler;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import java.awt.image.BufferedImage;
 import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class Gen3RomHandlerTest {
 
-    private static final String RUBY_U_FILENAME = "test/roms/Ruby (U).gba";
+    private static final String GBA_FORMAT = "test/roms/%s.gba";
 
-    private static final String[] ROMS_TO_TEST = {"Ruby (U)", "Ruby (S)", "Ruby (F)", "Emerald (J)"};
+    private static final String[] ALL_ROMS = {"Ruby (U)", "Ruby (S)", "Ruby (F)", "Emerald (U)", "Emerald (J)"};
 
     private final Gen3RomHandler.Factory factory = new Gen3RomHandler.Factory();
     private RomHandler romHandler;
 
-    @Before
-    public void loadRubyU() {
-        assertTrue(factory.isLoadable(RUBY_U_FILENAME));
+    public static String[] getROMNames() {
+        return ALL_ROMS;
+    }
+
+    private void loadROM(String romName) {
+        String fullRomName = String.format(GBA_FORMAT, romName);
+        if (!factory.isLoadable(fullRomName)) {
+            throw new IllegalArgumentException("ROM is not loadable.");
+        }
         romHandler = factory.create(new Random(), new PrintStream(System.out));
-        romHandler.loadRom(RUBY_U_FILENAME);
+        romHandler.loadRom(fullRomName);
     }
 
-    @Test
-    public void rubyUHasRightROMCodeAndName() {
-        assertEquals("AXVE", romHandler.getROMCode());
-        assertEquals("Ruby (U)", romHandler.getROMName());
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void loadingDoesNotGiveNullRomHandler(String romName) {
+        loadROM(romName);
+        assertNotNull(romHandler);
     }
 
-    @Test
-    public void getPokemon() {
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void pokemonIsNotEmpty(String romName) {
+        loadROM(romName);
+        assertFalse(romHandler.getPokemon().isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void firstPokemonIsNull(String romName) {
+        loadROM(romName);
         assertNull(romHandler.getPokemon().get(0));
-        assertEquals("BULBASAUR", romHandler.getPokemon().get(Species.bulbasaur).getName());
-        assertEquals("DEOXYS", romHandler.getPokemon().get(Species.deoxys).getName());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void numberOfPokemonEqualsGen3Constant(String romName) {
+        loadROM(romName);
         assertEquals(Gen3Constants.pokemonCount + 1, romHandler.getPokemon().size());
     }
 
-    @Test
-    public void getMoves() {
-        romHandler.getMoves().forEach(System.out::println);
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void movesAreNotNull(String romName) {
+        loadROM(romName);
+        assertNotNull(romHandler.getMoves());
     }
 
-    @Test
-    public void foobar() {
-        List<Move> before = new ArrayList<>(romHandler.getMoves());
-        romHandler.randomizeMoveTypes();
-        assertNotEquals(before, romHandler.getMoves());
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void movesAreNotEmpty(String romName) {
+        loadROM(romName);
+        System.out.println(romHandler.getMoves());
+        assertFalse(romHandler.getMoves().isEmpty());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getROMNames")
+    public void firstMoveIsNull(String romName) {
+        loadROM(romName);
+        assertNull(romHandler.getMoves().get(0));
     }
 
     // TODO: set up a framework for unit testing various different ROMs
