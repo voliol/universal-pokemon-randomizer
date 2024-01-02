7bd6873135ea20524eff7eac67210bc85813f930
voliol <30631214+voliol@users.noreply.github.com>
2024-01-02 09:24:44 +0100

    fix typo causing build failure

--- src/com/dabomstew/pkrandom/romhandlers/Gen2RomHandler.java
+++ src/com/dabomstew/pkrandom/romhandlers/Gen2RomHandler.java
@@ -2004,7 +2004,7 @@ public class Gen2RomHandler extends AbstractGBCRomHandler {
         for (Map.Entry<Integer, Shop> entry : shopItems.entrySet()) {
             int shopNum = entry.getKey();
             Shop shop = entry.getValue();
-            new GBDataRewriter<Shop>().rewriteData(tableOffset + shopNum * 2, shop, this::shopToBytes,
+            new GBCDataRewriter<Shop>().rewriteData(tableOffset + shopNum * 2, shop, this::shopToBytes,
                     this::lengthOfShopAt);
         }
     }
