b16ebe329806a597d0d85d614abb9bce4aa477a0
voliol <30631214+voliol@users.noreply.github.com>
2024-02-01 13:09:29 +0100

    continue testing loadfactors

--- test/pokemon/PokemonSetTest.java
+++ test/pokemon/PokemonSetTest.java
@@ -15,20 +15,27 @@ public class PokemonSetTest {
     // and e.g. a load factor of 0.75 means it should make a new ArrayList when only 75% of the elements remain.
     // The currently used PokemonSet implementation essentially works like the former.
 
-    private static final int[] AMOUNTS = new int[] {100, 200, 1000, 2000, 4000};
-    private static final double[] LOAD_FACTORS = new double[] {2, 0.0, 0.25, 0.5, 0.75};
+    private static final int[] AMOUNTS = new int[] {100, 200, 1000, 2000, 4000, 10000};
+    private static final double[] LOAD_FACTORS = new double[] {0.25, 1.0/Math.PI, 1.0/Math.E, 0.5};
     private static final int REPS = 1000;
 
     public static void main(String[] args) {
 
         for (int amount : AMOUNTS) {
-            for (double loadFactor : LOAD_FACTORS) {
 
-                long[] times = new long[REPS];
-                long maxTime = 0;
-                long minTime = Long.MAX_VALUE;
+            long[][] times = new long[LOAD_FACTORS.length][REPS];
+            long[] maxTimes = new long[LOAD_FACTORS.length];
+            long[] minTimes = new long[LOAD_FACTORS.length];
 
-                for (int rep = 0; rep < REPS; rep++) {
+            System.out.println("Amount: " + amount);
+
+            for (int rep = 0; rep < REPS; rep++) {
+
+                for (int lfi = 0; lfi < LOAD_FACTORS.length; lfi++) {
+                    double loadFactor = LOAD_FACTORS[lfi];
+
+                    maxTimes[lfi] = 0;
+                    minTimes[lfi] = Long.MAX_VALUE;
 
                     NewPokemonSet<Pokemon> pokes = new NewPokemonSet<>(loadFactor);
                     for (int pokeID = 0; pokeID < amount; pokeID++) {
@@ -40,28 +47,30 @@ public class PokemonSetTest {
                     Random random = new Random(rep);
                     long before = System.currentTimeMillis();
 
-                    while(pokes.size() > 0) {
+                    while (pokes.size() > 0) {
                         Pokemon picked = pokes.getRandom(random);
                         pokes.remove(picked);
                     }
 
-                    times[rep] = System.currentTimeMillis() - before;
-                    if (times[rep] > maxTime) maxTime = times[rep];
-                    if (times[rep] < minTime) minTime = times[rep];
-                }
+                    times[lfi][rep] = System.currentTimeMillis() - before;
+                    if (times[lfi][rep] > maxTimes[lfi]) maxTimes[lfi] = times[lfi][rep];
+                    if (times[lfi][rep] < minTimes[lfi]) minTimes[lfi] = times[lfi][rep];
 
-                System.out.println("Amount: " + amount);
-                System.out.println("LoadFactor: " + loadFactor);
-                double average = Arrays.stream(times).average().getAsDouble();
-                long sum = Arrays.stream(times).sum();
-                System.out.println("Average: " + average + "\tMax: " + maxTime + "\tMin: " + minTime + "\tSum: " + sum);
-                System.out.println("\n");
+                }
             }
 
+            for (int lfi = 0; lfi < LOAD_FACTORS.length; lfi++) {
+                System.out.println("LoadFactor: " + LOAD_FACTORS[lfi]);
+                double average = Arrays.stream(times[lfi]).average().getAsDouble();
+                long sum = Arrays.stream(times[lfi]).sum();
+                System.out.println("Average: " + average + "\tMax: " + maxTimes[lfi] + "\tMin: " + minTimes[lfi] + "\tSum: " + sum);
+            }
+            System.out.println("\n");
         }
 
         System.out.println("done");
     }
+
 
 
 }
