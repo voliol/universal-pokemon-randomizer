f1ae6df12cbd64a58cb244a796a7960174e5c038
voliol <30631214+voliol@users.noreply.github.com>
2024-04-09 14:37:14 +0200

    add test method to see all EvolutionTypes

--- test/test/romhandlers/RomHandlerEvolutionTest.java
+++ test/test/romhandlers/RomHandlerEvolutionTest.java
@@ -9,10 +9,7 @@ import com.dabomstew.pkrandom.pokemon.Pokemon;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
@@ -33,6 +30,39 @@ public class RomHandlerEvolutionTest extends RomHandlerTest {
             }
         }
         assertTrue(hasEvolutions);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getRomNames")
+    public void printAllEvoTypesByUsage(String romName) {
+        // not really a test since it makes no assertions, but still useful when debugging
+        loadROM(romName);
+
+        Map<EvolutionType, List<Evolution>> allEvos = new EnumMap<>(EvolutionType.class);
+        for (EvolutionType et : EvolutionType.values()) {
+            allEvos.put(et, new ArrayList<>());
+        }
+
+        for (Pokemon pk : romHandler.getPokemonSet()) {
+            for (Evolution evo : pk.getEvolutionsFrom()) {
+                allEvos.get(evo.getType()).add(evo);
+            }
+        }
+
+        Comparator<Map.Entry<EvolutionType, List<Evolution>>> comparator =
+                Comparator.comparingInt(entry -> entry.getValue().size());
+        comparator = comparator.reversed();
+        List<Map.Entry<EvolutionType, List<Evolution>>> sorted =
+                allEvos.entrySet().stream()
+                        .filter(entry -> entry.getValue().size() != 0)
+                        .sorted(comparator)
+                        .toList();
+        for (Map.Entry<EvolutionType, List<Evolution>> entry : sorted) {
+            System.out.println(entry.getValue().size() + "\t" + entry.getKey());
+            for (Evolution evo : entry.getValue()) {
+                System.out.println("\t" + evo);
+            }
+        }
     }
 
     @ParameterizedTest
